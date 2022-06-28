c5b3e8c7c535d1b3461b9497f5fffe78f74f7db3
Helen Xia <58539579+helenxiia@users.noreply.github.com>
2022-06-27 22:41:01 -0400

    new redeploy.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-#CHECK CURRENT PROCESSES AND KILLS IF EXISTS
-PROCESSES=$(pidof tmux)
-if [[ "$PROCESSES" -ne "" ]]; then
-    kill "$PROCESSES"
-fi
-
 #GETS THE PROJECT DIRECTORY
 PROJECT_DIR="/root/flask-blog"
 cd $PROJECT_DIR || exit
@@ -26,10 +20,9 @@ source python3-virtualenv/bin/activate
 
 pip install -r requirements.txt
 
-TMUX_SESSION="setup"
-COMMAND="flask run --host=0.0.0.0"
+systemctl daemon-reload
+systemctl restart myportfolio
+systemctl status myportfolio
 
-tmux new-session -d -s "$TMUX_SESSION" "$COMMAND"
-
-echo "name of tmux session is $TMUX_SESSION"
+echo "SERVER HAS RESTARTED"
 
